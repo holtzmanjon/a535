14da89a9a0a3553650fd05b32045d9591944763a
wcroberson <wcr2023@stanford.edu>
2023-08-31 13:58:38 -0600

    Started file for SNR calculation

---
+++ snr_calculator/snr_calculator.py
@@ -0,0 +1,18 @@
+# This file contains functions for predicting the SNR of an observation
+# given a spectral energy distribution and information about the
+# telescope
+
+import sys
+import numpy as np
+import astropy
+from astropy import units as u
+from astropy import constants as const
+import matplotlib.pyplot as plt
+
+
+def main():
+    pass
+
+
+if __name_ == "__main__":
+    main()
