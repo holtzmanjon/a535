9bf916fdecf07b071bf52fef6a1c3579cc0267f5
jaklusmeyer <jaklusmeyer@gmail.com>
2023-09-14 11:14:03 -0600

    Added very basic snr given time equation put in a lot of dummy variables, added holder functions for mirrors and detector response

--- snr_calculator/snr_calculator.py
+++ snr_calculator/snr_calculator.py
@@ -1,7 +1,6 @@
 # This file contains functions for predicting the SNR of an observation
 # given a spectral energy distribution and information about the
 # telescope
-
 import sys
 import numpy as np
 import astropy
@@ -9,6 +8,9 @@ from astropy import units as u
 from astropy.modeling import models
 from astropy import constants as const
 import matplotlib.pyplot as plt
+import scipy
+from scipy import stats
+from scipy.integrate import quad
 
 
 def generate_sed(wavelengths, mag, spec_type='bb5500'):
@@ -63,6 +65,10 @@ def atmos_transmission(wavelengths, model='flat0.8'):
 
     """
     transmission = np.ones(wavelengths.shape)
+    
+    #trans=scipy.interpolate.interp1d(wavelengths,airmass,fill_value="extrapolate")
+    #trans*=np.ones(data.shape)
+    
 
     if 'flat' in model:
         trans_value = float(model[4:])
@@ -122,6 +128,55 @@ def filter_transmission(wavelengths, filter='flat0.8'):
     return transmission
 
 
+def mirror_transmission(wavelengths, filter='flat0.8'):
+    """Generates the mirror transmission curve
+
+        Calculates the transmission curve of the mirror based on the model entered.
+        Currently only supports a flat model.
+
+        Parameters:
+            wavelengths (np.array): numpy array of the wavelengths (with astropy units) at
+                                    which the transmission curve should be calculated
+            filter (string): type of filter; currently only supports a flat transmission curve
+
+
+        Returns:
+            np.array: mirror transmission at the given wavelengths
+
+    """
+    transmission = np.ones(wavelengths.shape)
+
+    if 'flat' in filter:
+        trans_value = float(filter[4:])
+        transmission *= trans_value
+
+    return transmission
+
+def detector_response(wavelengths, filter='flat0.8'):
+    """Generates the detector response for given pixel location
+
+        Calculates the response curve of the detector based on the model entered.
+        Currently only supports a flat model.
+
+        Parameters:
+            wavelengths (np.array): numpy array of the wavelengths (with astropy units) at
+                                    which the transmission curve should be calculated
+            filter (string): type of filter; currently only supports a flat transmission curve
+
+
+        Returns:
+            np.array: detector response at the given wavelengths
+
+    """
+    response = np.ones(wavelengths.shape)
+
+    if 'flat' in filter:
+        trans_value = float(filter[4:])
+        response *= trans_value
+
+    return response
+
+
 def tele_area(name='APO'):
     """Gives telescope area based on telescope name
 
@@ -175,6 +230,52 @@ def calc_monochrome_signal(sed, transmission, area=(np.pi*(3.5/2*u.m)**2), exp_t
     monochrome_signal = (area * exp_time * sed * transmission).to(1/u.Angstrom)
 
     return monochrome_signal
+
+def snr_(mag,wavelengths,time):
+    """Calculates the snr of a given object and exposure time
+        !!fix later
+
+        Uses the signal equation, along with the given transmission curve, to calculate
+        the monochromatic signal at the given wavelengths. Wavelengths and sed should
+        have units using astropy.units.
+
+        Parameters:
+            sed (np.array): numpy array of the same size as wavelengths which contains
+                            the flux at the given wavelengths
+            transmission (np.array): transmission spectrum (combined to include any relevant factors
+                                     including atmosphere, detector effeciency, etc.)
+            area (Quantity): telescope area; defaults to the area of a telescope with diameter
+                                  3.5 m; can use 1 (with no units) to receive signal per area
+            exp_time (Quantity): exposure time for the observation; defaults to 100 s; can use 1 (with
+                                 no units) to receive photons per second rather than total photons
+
+
+        Returns:
+            np.array: numpy array of the same length as wavelengths of predicted photon counts
+                      at each wavelength
+
+    """
+    F0=3.63e-9 #ergs/cm2/s/ang STMAG at 5500
+    h=6.626176e-27 #ergs s
+    c=2.99792458e10 #cm/s
+    atmos_trans=atmos_transmission(wavelengths)
+    mirror_trans=mirror_transmission(wavelengths)
+    instrument_trans=instrument_transmission(wavelengths)
+    filter_trans = filter_transmission(wavelengths)
+    detect_respon = detector_response(wavelengths)
+    F=F0*10**(-0.4*mag)
+    integral=quad(F,wavelengths[0],wavelengths[-1])
+    signal=tele_area*time
+    aperature=3
+    background_area=aperature
+    surf_bright=1
+    rn_sigma=3
+    platescale=3
+    Npix=platescale*background_area
+    snr=signal/np.sqrt(signal+background_area*surf_bright+rn_sigma*Npix)
+    
+    return snr
+    
 
 
 def main():
