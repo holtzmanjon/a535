5d857d949123904b45e7d60eff68a274b7df58a5
wcroberson <wcr2023@stanford.edu>
2023-09-26 10:19:04 -0600

    Fixed issues in the background and t from snr, and added options for using instrumental magnitudes

--- snr_calculator/snr_calculator.py
+++ snr_calculator/snr_calculator.py
@@ -37,36 +37,42 @@ def generate_sed(wavelengths, mag, spec_type='bb5500'):
     """
     sed = None
     sed = None
-    planet='fagk81d266.dat.txt'  
-    header_row = 'wave, flux, extra 3\n'    
-    # Open the file for reading
-    with open(planet, 'r') as file:
-        # Read the existing content
-        existing_content = file.read()  
-    if header_row not in existing_content:
-        updated_content = header_row + existing_content
-        with open(planet, 'w') as file:
-            file.write(updated_content)
-        print("file updated with header")
-    else:
-        print("")
-    star = pd.read_csv(planet, delimiter='\s+', skiprows=1, header=None, names=['wave', 'flux', 'extra'])
-    #print(star.head())    
-    wave = star['wave'].to_numpy()
-    flux=star['flux'].to_numpy() #flux ( ergs/cm/cm/s/A * 10**16 ) maybe better to call specific intensity
-    interpolated_flux = interp1d(wave, flux, kind='linear', fill_value="extrapolate")
-    desired_flux=interpolated_flux(wavelengths) #flux density need to integrate over wavelength
-    scaled_flux=desired_flux*10**(-0.4 * mag)
-    sed1 = trapz(scaled_flux, x=wavelengths)# ergs/cm/cm/s  FLUXlamda
-    #scale=sed1** 10**(-0.4 * mag)
-    energy = const.c * const.h / wavelengths
-    sed = sed1 / energy
-    if 'bb' in spec_type:
+    if spec_type == 'star':
+        planet='fagk81d266.dat.txt'
+        header_row = 'wave, flux, extra 3\n'
+        # Open the file for reading
+        with open(planet, 'r') as file:
+            # Read the existing content
+            existing_content = file.read()
+        if header_row not in existing_content:
+            updated_content = header_row + existing_content
+            with open(planet, 'w') as file:
+                file.write(updated_content)
+            print("file updated with header")
+        else:
+            print("")
+        star = pd.read_csv(planet, delimiter='\s+', skiprows=1, header=None, names=['wave', 'flux', 'extra'])
+        #print(star.head())
+        wave = star['wave'].to_numpy()
+        flux=star['flux'].to_numpy() #flux ( ergs/cm/cm/s/A * 10**16 ) maybe better to call specific intensity
+        interpolated_flux = interp1d(wave, flux, kind='linear', fill_value="extrapolate")
+        desired_flux=interpolated_flux(wavelengths) #flux density need to integrate over wavelength
+        scaled_flux=desired_flux*10**(-0.4 * mag)
+        sed1 = trapz(scaled_flux, x=wavelengths)# ergs/cm/cm/s  FLUXlamda
+        #scale=sed1** 10**(-0.4 * mag)
+        energy = const.c * const.h / wavelengths
+        sed = sed1 / energy
+    elif 'bb' in spec_type:
         temp = float(spec_type[2:]) * u.K
         bb = models.BlackBody(temperature=temp, scale=1*u.J*u.m**-2/u.s/u.micron/u.sr)
         norm_factor = (3.63e-9*u.erg/u.cm**2/u.s/u.Angstrom) / (bb(5500 * u.Angstrom) * u.sr) * 10**(-0.4 * mag)
         energy = const.c * const.h / wavelengths
         sed = bb(wavelengths) * norm_factor * u.sr / energy
+    elif 'instmag' in spec_type:
+        inst_mag = float(spec_type[7:])
+        flux = 10 ** (-inst_mag / 2.5) / u.s  # in photons per second
+        sed = np.ones(wavelengths.shape) * flux / (np.max(wavelengths) - np.min(wavelengths))
+
     else:
         print('Spectral type currently unsupported. Null value returned.')
 
@@ -178,6 +184,7 @@ def mirror_transmission(wavelengths, filter='flat0.8'):
 
     return transmission
 
+
 def detector_response(wavelengths, filter='flat0.8'):
     """Generates the detector response for given pixel location
 
@@ -230,7 +237,7 @@ def tele_area(name='APO'):
     return area
 
 
-def calc_monochrome_signal(sed, transmission, area=(np.pi*(3.5/2*u.m)**2), exp_time=(100*u.s)):
+def calc_monochrome_signal(sed, transmission, area=(np.pi*(3.5/2*u.m)**2), exp_time=(100*u.s), use_area=True):
     """Calculates the monochromatic flux of the given SED
 
         Uses the signal equation, along with the given transmission curve, to calculate
@@ -253,6 +260,8 @@ def calc_monochrome_signal(sed, transmission, area=(np.pi*(3.5/2*u.m)**2), exp_t
                       at each wavelength
 
     """
+    if not use_area:
+        area = 1
     monochrome_signal = (area * exp_time * sed * transmission)
     try:
         monochrome_signal = monochrome_signal.to(1/u.Angstrom)
@@ -262,7 +271,7 @@ def calc_monochrome_signal(sed, transmission, area=(np.pi*(3.5/2*u.m)**2), exp_t
     return monochrome_signal
 
 
-def atmos_emission(wavelengths):
+def atmos_emission(wavelengths, type='eso', area=(np.pi*(3.5/2*u.m)**2)):
     """Calculate emission from the atmosphere
 
             Uses an ESO sky model to calculate the emission due to the atmosphere at the given
@@ -275,17 +284,26 @@ def atmos_emission(wavelengths):
     Returns:
         np.array: array with astropy units of the sky emission at the given wavelengths
     """
-    lambdas = []
-    fluxes = []
-    with open('sky_0.0.csv', 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            data = line.split(',')
-            lambdas.append(data[0])
-            fluxes.append(data[1])
-    lambdas = np.array(lambdas) * u.nm
-    fluxes = np.array(fluxes) / u.m**2 / u.s / u.micron
-    new_fluxes = np.interp(wavelengths, lambdas, fluxes)
+    if type == 'eso':
+        lambdas = []
+        fluxes = []
+        with open('sky_0.0.csv', 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                data = line.split(',')
+                lambdas.append(data[0])
+                fluxes.append(data[1])
+        lambdas = np.array(lambdas) * u.nm
+        fluxes = np.array(fluxes) / u.m**2 / u.s / u.micron
+        new_fluxes = np.interp(wavelengths, lambdas, fluxes) * area
+    elif 'instmag' in type:
+        inst_mag = float(type[7:])
+        bg = 10**(-inst_mag/2.5) / u.s  # photons per second per square arcsec
+        new_fluxes = np.ones(wavelengths.shape) * bg / (np.max(wavelengths) - np.min(wavelengths))
+    else:
+        print("Type not currently supported")
+        new_fluxes = 0
+
     return new_fluxes
 
 
@@ -342,8 +360,8 @@ def exp_snr_from_time(sed, transmission, wavelengths,time,tele_area=(np.pi*(3.5/
     return snr
     
 
-def exp_time_from_snr(snr, sed, transmission, wavelengths, tele_area=(np.pi*(3.5/2*u.m)**2),
-                      bg_area=np.pi, platescale=2, read_noise=.2, monochrome=True):
+def exp_time_from_snr(snr, sed, transmission, wavelengths, B_monochrome, tele_area=(np.pi*(3.5/2*u.m)**2),
+                      bg_area=np.pi, platescale=2, read_noise=5, monochrome=True):
     """Calculates the the exposure time needed to get the required SNR
 
             Uses the signal equation, along with the given transmission curve, to calculate
@@ -357,6 +375,7 @@ def exp_time_from_snr(snr, sed, transmission, wavelengths, tele_area=(np.pi*(3.5
                 transmission (np.array): transmission spectrum (combined to include any relevant factors
                                          including atmosphere, detector effeciency, etc.)
                 wavelengths (np.array): numpy array of wavelengths corresponding to the SED
+                B_monochrome (np.array): background photons per wavelength per time
                 tele_area (Quantity): telescope area; defaults to the area of a telescope with diameter
                                       3.5 m; can use 1 (with no units) to receive signal per area
                 bg_area (float): area (in square arcseconds) of the aperature over which the stars light
@@ -370,20 +389,18 @@ def exp_time_from_snr(snr, sed, transmission, wavelengths, tele_area=(np.pi*(3.5
         """
     if monochrome:
         S = calc_monochrome_signal(sed, transmission, area=tele_area, exp_time=1)
-        B = atmos_emission(wavelengths) * tele_area
         a = -S ** 2
-        b = snr ** 2 * (S + bg_area * B)
+        b = snr ** 2 * (S + bg_area * B_monochrome)
         c = snr ** 2 * platescale * bg_area * read_noise
         exp_time = ((-b - np.sqrt(b ** 2 - 4 * a * c)) / (2 * a)).to(u.s)
     else:
         S_monochrome = calc_monochrome_signal(sed, transmission, area=tele_area, exp_time=1)
         S = 0 / u.s
-        B_monochrome = atmos_emission(wavelengths) * tele_area
         B = 0 / u.s
         for i in range(len(S_monochrome)-1):
             delta_lambda = wavelengths[i + 1] - wavelengths[i]
             S += delta_lambda * S_monochrome[i]
-            B += delta_lambda * S_monochrome[i]
+            B += delta_lambda * B_monochrome[i]
         a = -S**2
         b = snr**2 * (S + bg_area * B)
         c = snr**2 * platescale*bg_area*read_noise
@@ -392,18 +409,19 @@ def exp_time_from_snr(snr, sed, transmission, wavelengths, tele_area=(np.pi*(3.5
 
 
 def main():
-    wavelengths = 5500 * u.Angstrom
+    wavelengths = (5500, 5550) * u.Angstrom
     V_mag = 20
     atmos_trans = atmos_transmission(wavelengths)
     filter_trans = filter_transmission(wavelengths)
     instrument_trans = instrument_transmission(wavelengths)
-    transmission = atmos_trans * filter_trans * instrument_trans
+    transmission = np.array([1,1])  # atmos_trans * filter_trans * instrument_trans
     area = tele_area(name='rad0.3')
-    photon_sed = generate_sed(wavelengths, V_mag)
-    signal = calc_monochrome_signal(photon_sed, transmission, area=area, exp_time=(1*u.s))
-    print(signal, 'signal')
-    print(signal*850*u.Angstrom)
-    #print(exp_time_from_snr(100,signal,transmission,wavelengths), 'exposure time from snr')
+    photon_sed = generate_sed(wavelengths, V_mag, spec_type='instmag-8.5')
+    bg = atmos_emission(wavelengths, type='instmag-2.5')
+    #signal = calc_monochrome_signal(photon_sed, transmission, area=area, exp_time=(1))
+    #print(signal, 'signal')
+    #print(signal*850*u.Angstrom)
+    print(exp_time_from_snr(100,photon_sed,transmission,wavelengths, bg, monochrome=False), 'exposure time from snr')
     #exp_snr_from_time
     #print(exp_snr_from_time(100,photon_sed*u.s,transmission,wavelengths), 'snr from time')
 
